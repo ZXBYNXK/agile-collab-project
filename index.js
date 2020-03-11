532507d6ca4936a17e3924da2eee452b72497e6e
johnfruscella <johnfruscella@gmail.com>
2020-03-11 17:43:49 -0400

    Added index.js file and edited package.json's scripts section

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-require('dotenv/config');
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const DB_CONNECTION = process.env.DB_CONNECTION;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const enviroment =  process.env.NODE_ENV
-const path = require('path')
-const graduatesRoute = require('./routes/graduates');
+const graduatesRoute = require('./routes/jzgraduates');   
 enviroment.toLowerCase()
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-mongoose.connect(process.env.DB_CONNECTION,
-    { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(console.log("Connected to MongoDB"))
-    .catch(err => console.log(err));
+app.use('/jzgraduates', graduatesRoute) .
 
-    app.use('/api/graduates/', graduatesRoute);
+app.get('/', (req, res) => {
+    res.status(200).render('index')
+    
+  })
+  
+  
+  mongoose.connect(DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => { 
+      console.log(`Database: ${DB_CONNECTION}`)     
+  })
+    .catch(error => console.log("DB Connection error", error));
+    
+    app.use(express.static("public"));
+  
+  
+  app.listen(PORT, () => { 
+  console.log(`Server listening on port ${PORT}`)
+  });
 
-app.listen(PORT, () => console.log(`Server is currently listening on PORT ${PORT}`))
+
+  

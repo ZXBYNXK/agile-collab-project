57941e50eb0e4685521ed99414ec3448cdc9b338
DariusRain <dariusrain.student@careerdevs.com>
2020-03-21 15:43:55 -0400

    Created a default redirects to home route

--- index.js
+++ index.js
@@ -9,21 +9,34 @@ const articleRoute = require('./routes/articleRoutes');
 
 enviroment.toLowerCase()
 app.use(express.json());
+app.set('view engine', 'pug')
+
+
+
 app.use('/graduateRoutes', graduatesRoute)
 app.use('/articleRoutes', articleRoute)
 
 
 
-  
-  
+
   mongoose.connect(DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
     .then(() => { 
       console.log(`Database: ${DB_CONNECTION}`)     
   })
     .catch(error => console.log("DB Connection error", error));
-    
+
+
+  
   
     app.use(express.static("public"));
+  
+  
+  //DR: This redirects back to home if no route handlers find a solution. 
+    app.use( (req, res, next) => {
+      res.redirect('/')
+    })
+    
+
 
   app.listen(PORT, () => { 
   console.log(`Server listening on port ${PORT}`)
